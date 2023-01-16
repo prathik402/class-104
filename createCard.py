95d1289b42069faf8f005f024b4bcd0ef3cec38a
prathik402 <73998548+prathik402@users.noreply.github.com>
2023-01-16 17:27:58 +0530

    Add files via upload

---
+++ createCard.py
@@ -0,0 +1,8 @@
+import cv2
+img = cv2.imread("poster.jpg")
+rocket = img[120:360,400:500]
+img[0:240,500:600] = rocket
+textToShow = "Prathik"
+cv2.putText(img,textToShow,(20,200),fontFace = cv2.FONT_HERSHEY_COMPLEX_SMALL,fontScale = 1, color =(0,0,255))
+cv2.imshow("output",img)
+cv2.waitKey(0)
